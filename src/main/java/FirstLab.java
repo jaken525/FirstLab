786246eeae0bc9e689e7021189c8acfca5e35ebd
Vitaliy Povazhnyy <57368196+jaken525@users.noreply.github.com>
2025-10-06 22:09:02 +0500

    fix

--- src/main/java/FirstLab.java
+++ src/main/java/FirstLab.java
@@ -2,6 +2,6 @@ public class FirstLab {
     public static void main(String[] args) {
         System.out.println("Поважный Виталий Евгеньевич РИС-22-1б");
         System.out.println("Зырянов Ростислав Александрович РИС-22-1б");
-		System.out.println("Вышенская Екатерина Игоревна РИС-22-1б");
+        System.out.println("Вышенская Екатерина Игоревна РИС-22-1б");
     }
 }
