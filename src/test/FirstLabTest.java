4b3ee6a8580061a339d83528ecae991cc0709024
Povazhnyy Vitaliy <vitaliy.povazhnyy.93@mail.ru>
2025-10-06 17:46:51 +0500

    Add test and update pipes

---
+++ src/test/FirstLabTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class FirstLabTest {
+    @Test
+    public void testGreeting() {
+        assertEquals("Поважный Виталий Евгеньевич РИС-22-1б", "Поважный Виталий Евгеньевич РИС-22-1б");
+    }
+}
