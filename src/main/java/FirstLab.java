00c938b42f9994ae155a0f76da94793a31cd7bc9
Povazhnyy Vitaliy <vitaliy.povazhnyy.93@mail.ru>
2025-10-21 01:26:52 +0500

    Переписал прогу с тестами и обновил пайпы

--- src/main/java/FirstLab.java
+++ src/main/java/FirstLab.java
@@ -10,6 +10,12 @@ public class FirstLab {
 
     public static void main(String[] args) {
         printGreetings(System.out);
+		
+        Calculator calc = new Calculator();
+        System.out.println("Addition: " + calc.add(5.0, 3.0));
+        System.out.println("Subtraction: " + calc.subtract(5.0, 3.0));
+        System.out.println("Multiplication: " + calc.multiply(5.0, 3.0));
+        System.out.println("Division: " + calc.divide(6.0, 2.0));
     }
 
     public static void printGreetings(PrintStream out) {
